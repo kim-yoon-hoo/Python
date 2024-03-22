ab2ef3d4011eb22a244c676ff7ccd454c5451708
kim-yoon-hoo <162937261+kim-yoon-hoo@users.noreply.github.com>
2024-03-22 14:14:17 +0900

    Add files via upload

---
+++ test_1도형.py
@@ -0,0 +1,89 @@
+# 거북이 그래픽 모듈 사용 - 한줄 주석
+
+import turtle #모듈울 내 프로그램에서 사용
+#import turtle as t #별명을 정해줌
+
+turtle.shape("turtle") #circle 동그라미 #arrow 화살표 #turtle 거북이 #square 사각형
+
+turtle.color("purple")
+turtle.begin_fill()
+#팔각형 그리기
+turtle.forward(100)
+turtle.left(45)
+turtle.forward(100)
+turtle.left(45)
+turtle.forward(100)
+turtle.left(45)
+turtle.forward(100)
+turtle.left(45)
+turtle.forward(100)
+turtle.left(45)
+turtle.forward(100)
+turtle.left(45)
+turtle.forward(100)
+turtle.left(45)
+turtle.forward(100)
+turtle.left(45)
+turtle.end_fill()
+
+
+
+turtle.color("blue")
+turtle.begin_fill()
+#육각형 그리기
+turtle.forward(100)
+turtle.left(60)
+turtle.forward(100)
+turtle.left(60)
+turtle.forward(100)
+turtle.left(60)
+turtle.forward(100)
+turtle.left(60)
+turtle.forward(100)
+turtle.left(60)
+turtle.forward(100)
+turtle.left(60)
+turtle.end_fill()
+
+turtle.color("red")
+turtle.begin_fill()
+#오각형 그리기
+turtle.forward(100)
+turtle.left(72)
+turtle.forward(100)
+turtle.left(72)
+turtle.forward(100)
+turtle.left(72)
+turtle.forward(100)
+turtle.left(72)
+turtle.forward(100)
+turtle.left(72)
+turtle.end_fill()
+
+
+turtle.color("green")
+turtle.begin_fill()
+#사각형 그리기
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+turtle.left(90)
+turtle.end_fill()
+
+turtle.color("orange")
+turtle.begin_fill()
+#삼각형 그리기
+turtle.forward(100)
+turtle.left(120)
+turtle.forward(100)
+turtle.left(120)
+turtle.forward(100)
+turtle.left(120)
+turtle.end_fill()
+
+
+
---
+++ test_2별.py
@@ -0,0 +1,17 @@
+#터틀 별그리기
+import turtle
+turtle.shape("turtle")
+
+turtle.color("red")
+turtle.begin_fill()
+turtle.forward(200)
+turtle.left(144)
+turtle.forward(200)
+turtle.left(144)
+turtle.forward(200)
+turtle.left(144)
+turtle.forward(200)
+turtle.left(144)
+turtle.forward(200)
+turtle.left(144)
+turtle.end_fill()
