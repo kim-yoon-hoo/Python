8442dd7a07e555ff912adf1aea1cd0a5f1d15ab9
kim-yoon-hoo <162937261+kim-yoon-hoo@users.noreply.github.com>
2024-04-05 14:32:07 +0900

    Add files via upload

---
+++ str_test.py
@@ -0,0 +1,34 @@
+#변수 자료를 담는 공간
+#예약어 if for in True False
+#함수 자주 사용하는 기능 내장함수 print() type()
+#연산자 산술:+ -  대입: =  비교(관계): > <  논리(and or not)
+#자료형 정수int 실수float 문자str
+#이스케이프코드 줄바꿈 \n
+#문자열 다루기 인덱싱
+#문자열 함수
+n = "Happy Programming!"
+print(n)
+m = n[0:5]
+print(m)
+print(n[-1])
+print(n[-4:-1])
+print(n[6:10])
+
+print(len(n)) #글자수를 세주는 중요한 명령어 (필수)
+print(n.upper()) #대문자로 바꿔주는 명령어
+name = "제 이름은 김윤후입니다"
+name_e ="my name is yoonhoo"
+
+
+print(len(name_e))
+print(name_e.upper())
+print(name_e.lower())
+
+
+
+
+
+
+
+
+
